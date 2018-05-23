11106a63998c58b6565e460ce7d38b7db4b0e64a
ZETYSX\Administrator <quyetnp@fsoft.com>
2018-05-23 23:16:05 +0700

    Add welcome screen

---
+++ app/src/main/java/com/example/admin/managerstundent/Activity/WelcomeActivity.java
@@ -0,0 +1,34 @@
+package com.example.admin.managerstundent.Activity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.admin.managerstundent.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private static final int SWITCH_SCREEN = 3000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SWITCH_SCREEN);
+    }
+}
